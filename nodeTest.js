44a875975806354a0670d756151ae5294aaf7dbf
easyblend <kenzieemma072@gmail.com>
2023-01-12 19:28:01 +0000

    playing with http module in node js

--- nodeTest.js
+++ nodeTest.js
@@ -1,36 +1,61 @@
-//Getting Serious with classes and OOP
+// //Getting Serious with classes and OOP
 
-class Person {
-  constructor(height, complextion, age) {
-    this.height = height;
-    this.complextion = complextion;
-    this.age = age;
-  }
+// class Person {
+//   constructor(height, complextion, age) {
+//     this.height = height;
+//     this.complextion = complextion;
+//     this.age = age;
+//   }
 
-  animatePerson() {
-    console.log(
-      `Mark is ${this.height}cm tall and he is ${this.complextion} in complextion`
-    );
-  }
+//   animatePerson() {
+//     console.log(
+//       `Mark is ${this.height}cm tall and he is ${
+//         this.complextion
+//       } in complextion  ${
+//         this.YearOfStudy ? "and he is in " + this.YearOfStudy : ""
+//       }`
+//     );
+//   }
 
-  //A static Method to check if two person instatiated from the Person class are the same
-  static checkProperties(person_a, person_b) {
-    return (
-      person_a.height === person_b.height &&
-      person_a.complextion === person_b.complextion &&
-      person_a.age === person_b.age
-    );
-  }
+//   //A static Method to check if two person instatiated from the Person class are the same
+//   // static checkProperties(person_a, person_b) {
+//   //   return (
+//   //     person_a.height === person_b.height &&
+//   //     person_a.complextion === person_b.complextion &&
+//   //     person_a.age === person_b.age
+//   //   );
+//   // }
 
-  //Using setter to create a method
-  // set person(height) {
-  //   this.height = height;
+//   //Using setter to create a method
+//   // set person(height) {
+//   //   this.height = height;
 
-  //   console.log(`New height is ${this.height}`);
-  // }
-}
+//   //   console.log(`New height is ${this.height}`);
+//   // }
+// }
 
-const person_1 = new Person(182, "light skin", 21);
-const persom_2 = new Person(182, "light skin", 21);
+// //A child classs that inherits from The person class
+// class Student extends Person {
+//   constructor(height, complextion, age, YearOfStudy) {
+//     super(height, complextion, age);
+//     this.YearOfStudy = YearOfStudy;
+//   }
+// }
 
-console.log(Person.checkProperties(persom_2, person_1));
+// //Creating two instance objects from the Person's Class
+// const person_1 = new Person(182, "light skin", 21);
+// // const persom_2 = new Person(182, "light skin", 21);
+
+// const student_1 = new Student(177, "Chocolate", 20, "3rd year");
+
+// student_1.animatePerson();
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.write("Hellos Node world");
+  res.end();
+});
+
+server.listen(1000);
